c7aeb7b4685fc71dc63abe0d096b1abec8e0dbbc
linmingxiong <272921246@qq.com>
2017-03-07 16:43:28 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,43 @@
+var data = {
+  leaver1: [{
+    title:'',
+    date:'',
+    authod:'',
+    context:"XXXXXXXXXXX，XXXXXXXXX。",
+    contextTable: "XXXXXXXXXXXX",
+    source: "秋离"
+  }, {
+    title:'',
+    date:'',
+    authod:'',
+    context:"XXXXXXXXXXX，XXXXXXXXX。",
+    contextTable: "XXXXXXXXXXXX"
+  }, {
+    title:'',
+    date:'',
+    authod:'',
+    context:"XXXXXXXXXXX，XXXXXXXXX。",
+    contextTable: "XXXXXXXXXXXX"
+  }],
+
+  leaver2: [{
+    title:'',
+    date:'',
+    authod:'',
+    context:"XXXXXXXXXXX，XXXXXXXXX",
+    contextTable: "XXXXXXXXXXXX"
+  }, {
+    title:'',
+    date:'',
+    authod:'',
+    context:"XXXXXXXXXXX，XXXXXXXXX。",
+    contextTable: "XXXXXXXXXXXX"
+  } {
+    title:'',
+    date:'',
+    authod:'',
+    context:"XXXXXXXXXXX，XXXXXXXXX。",
+    contextTable: "XXXXXXXXXXXX"
+  }]
+}
+console.log(data)
